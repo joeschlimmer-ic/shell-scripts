47975d9d09f730cb708dfa5db884c2b34ce9e7f9
Joe Schlimmer <joseph.schlimmer@gmail.com>
2022-01-26 08:00:41 -0500

    Support activating nvivo 12 and 20

--- nvivo-activate.sh
+++ nvivo-activate.sh
@@ -3,8 +3,12 @@
 # deactivate existing license and activate with new license
 # Joe Schlimmer Ithaca College 1-2-2020
 # 8-28-2020: Added more verbose script feedback lines
+# 2022-01-26: Revise to support both nvivo 12 and 20 activation
+# Use in Jamf Pro policy, make sure to set the 4 and 5 parameters
+# Version value can be 12 or 20
 
 license="$4"
+version="$5"
 
 cat > /tmp/license_data.xml << EOF
 <?xml version="1.0" encoding="utf-8" standalone="yes"?>
@@ -28,8 +32,20 @@ cat > /tmp/license_data.xml << EOF
 </Activation>
 EOF
 
-echo "Removing any existing Nvivo licenses"
-"/Applications/NVivo 12.app/Contents/MacOS/NVivo 12" -deactivate
+if [[ "$version" == 12 ]]; then
+    nvivo="/Applications/NVivo.app/Contents/MacOS/NVivo 12"
+elif [[ "$version" == 20 ]]; then
+    nvivo="/Applications/NVivo.app/Contents/MacOS/NVivo"
+else
+    echo "Version not specified or unknown"
+    exit 1
+fi
 
-echo "Activating Nvivo 12"
-"/Applications/NVivo 12.app/Contents/MacOS/NVivo 12" -initialize "$license" -activate /tmp/license_data.xml
+
+echo "Removing any existing Nvivo licenses"
+"$nvivo" -deactivate
+
+echo "Activating Nvivo"
+"$nvivo" -initialize "$license" -activate /tmp/license_data.xml
+
+exit 0

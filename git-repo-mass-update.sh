e2890bc5e091edad51891314bce8fff8d7c86cc1
Joe Schlimmer <joseph.schlimmer@gmail.com>
2022-02-03 08:53:59 -0500

    Quick 'n dirty script to git pull every repo in a parent dir

---
+++ git-repo-mass-update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+path="$1"
+
+if [[ ! -z "$path" ]]; then
+    pushd "$(pwd)"
+    cd "$path"
+    for item in *; do
+        if [[ -d "$item" ]]; then
+            cd $item
+            pwd
+            git pull
+            cd ..
+        else
+            continue
+        fi
+    done
+    popd
+else
+    echo "Must specifiy a parent directory to operate on. (., dirname)"
+fi
+
+exit $?

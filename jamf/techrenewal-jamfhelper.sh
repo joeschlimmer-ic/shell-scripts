76d1f5922d2776120fbbae70b9b313b4659146ef
Joe Schlimmer <joseph.schlimmer@gmail.com>
2022-02-18 17:02:17 -0500

    Extend jamf helper script, .gitignore

--- jamf/techrenewal-jamfhelper.sh
+++ jamf/techrenewal-jamfhelper.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+computerSerial="$(ioreg -c IOPlatformExpertDevice -d 2 | awk -F\" '/IOPlatformSerialNumber/{print $(NF-1)}')"
+
 # This sets the variable for jamfHelper results from which button was clicked
-
 buttonClicked=$("/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper" \
-    -windowType utility -icon "/Library/Ithaca/IC-Logo2L.png" -title "Ithaca College Information Technology" \
-    -heading "Ithaca College Tech Renewal Alert" -description "Ithaca College Tech Renewal Alert" \
-    -button1 "Ok" -button2 "Cancel")
+    -windowType utility \
+    -icon "/Library/Ithaca/IC-Logo2L.png" \
+    -title "Ithaca College Information Technology" \
+    -heading "This computer $computerSerial is scheduled for Technology Renewal" \
+    -description "Please schedule the renewal or contact the Ithaca College Service Desk at 607-274-1000" \
+    -button1 "OK" \
+    -button2 "Cancel")
 
-if [ $buttonClicked == 0 ]; then
+if [ "$buttonClicked" == 0 ]; then
 	# Buttion 1 was Clicked
-	echo "OK"
-        open "https://ithaca.edu"
-elif [ $buttonClicked == 2 ]; then
+        echo "$(date)::: Opening Scheduling page" >> /var/log/tech_renewal.log
+        echo "Your Ithaca College Mac Serialnumber: $computerSerial" >> /var/log/tech_renewal.log
+        open "https://bit.ly/ICTechRenewal"
+elif [ "$buttonClicked" == 2 ]; then
 	# Buttion 2 was Clicked
-	echo "Cancel"
+        echo "$(date)::: I dont want to right now" >> /var/log/tech_renewal.log
 fi
+exit $?

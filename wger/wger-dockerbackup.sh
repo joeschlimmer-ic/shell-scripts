b9dc9f156c21237b4c1064a7b86d0fee4cb9599d
Joe Schlimmer <joe@noreply.localhost>
2022-02-19 10:27:39 -0500

    Add time to filename string. Change backup dest path

--- wger/wger-dockerbackup.sh
+++ wger/wger-dockerbackup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-today=$(date +"%Y-%m-%d")
+today=$(date +%Y-%m-%d-T%H%M)
 
 docker stop docker_cache_1
 docker stop docker_nginx_1
 docker stop docker_db_1
 docker stop docker_web_1
 
-docker run --rm --volumes-from docker_db_1 -v /home/joe:/wgerbackup ubuntu bash -c "cd /var/lib/postgresql/data && tar cvf /wgerbackup/$today-wger-db-backup.tar ."
-docker run --rm --volumes-from docker_web_1 -v /home/joe:/wgerbackup ubuntu bash -c "cd /home/wger/media && tar cvf /wgerbackup/$today-wger-media-backup.tar . ; cd /home/wger/static && tar cvf /wgerbackup/$today-wger-static-backup.tar ."
+docker run --rm --volumes-from docker_db_1 -v /home/joe/backup:/wgerbackup ubuntu bash -c "cd /var/lib/postgresql/data && tar cvf /wgerbackup/$today-wger-db-backup.tar ."
+docker run --rm --volumes-from docker_web_1 -v /home/joe/backup:/wgerbackup ubuntu bash -c "cd /home/wger/media && tar cvf /wgerbackup/$today-wger-media-backup.tar . ; cd /home/wger/static && tar cvf /wgerbackup/$today-wger-static-backup.tar ."
 
 docker start docker_db_1
 docker start docker_web_1

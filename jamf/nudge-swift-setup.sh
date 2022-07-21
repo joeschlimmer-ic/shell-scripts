3eaf4e829fb295f1542971403e52822798258c49
Joe Schlimmer <joseph.schlimmer@gmail.com>
2022-07-21 15:33:31 -0400

    MIT license and copywrite

--- jamf/nudge-swift-setup.sh
+++ jamf/nudge-swift-setup.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+: <<-LICENSE_BLOCK
+nudge-swift-setup (20220721) - Copyright (c) 2021 Joe Schlimmer (https://github.com/wartz)
+Licensed under the MIT License
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+LICENSE_BLOCK
+
 # Installs the nudge LaunchAgent and runs an initial check for updates
+# Joe Schlimmer
+# 2022-07-21
 
 log() {
     echo "$1"

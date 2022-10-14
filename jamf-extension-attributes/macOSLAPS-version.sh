8ce4d8b2277ba555d14f6434a3d7fc129f9c9cac
Joe Schlimmer <joseph.schlimmer@gmail.com>
2022-10-14 15:25:52 -0400

    Detect laps 1.0, 2.0 and 3.0 in a single EA

--- jamf-extension-attributes/macOSLAPS-version.sh
+++ jamf-extension-attributes/macOSLAPS-version.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Check to see if LAPS is installed and send the version to jamf EA
+# Check to see if LAPS is installed and sends the version to jamf EA
 # Joe Schlimmer
 # Last Updated: 2022-10-14
 
-laps="/usr/local/laps/macOSLAPS"
-
-if [ -e "$laps" ] ; then
+if [ -e "/usr/local/laps/macOSLAPS" ] ; then
     echo "<result>$(/usr/local/laps/macOSLAPS -version)</result>"
+elif [ -e "/usr/local/bin/laps/macOSLAPS" ] ; then
+    echo "<result>$(/usr/local/bin/laps/macOSLAPS -version)</result>"
 else
 	echo "<result>Not Installed</result>"
 fi
